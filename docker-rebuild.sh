f2a96b27a200c850d27f60c39df4e348275a86c1
Aliatin Maksym <alyatin.max@gmail.com>
2019-11-26 22:59:31 +0200

    Move product Wishlist/Compare/Email links

--- docker-rebuild.sh
+++ docker-rebuild.sh
@@ -9,3 +9,4 @@ docker volume prune -f
 docker-compose -f docker-compose.yml -f docker-compose-dev.yml up -d --build --force-recreate
 # docker-compose -f ./docker-compose.yml -f ./docker-compose-dev.yml up -d
 # docker-sync-stack start
+grunt clean:alyatinmax_maxtheme_en_US && grunt exec:alyatinmax_maxtheme_en_US && grunt less:alyatinmax_maxtheme_en_US && grunt watch
